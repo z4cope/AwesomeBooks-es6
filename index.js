3c40ae93cb2bc73936eec0c2bf43202e1d141e84
z4cope <63551733+z4cope@users.noreply.github.com>
2022-10-31 20:04:20 +0200

    Fix linters errors

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 // Render of the books in the DOM
-import BooksInDom from "./modules/renderBooksFromDom.js";
+import BooksInDom from './modules/renderBooksFromDom.js';
 // Add Books to Ui
-import addBooksToUi from "./modules/addBooksToUi.js";
+import addBooksToUi from './modules/addBooksToUi.js';
 // Getting & setting books from & to local storage
-import BooksAtLocalStorage from "./modules/booksAtLs.js"; // eslint-disable-line no-unused-vars
+import BooksAtLocalStorage from './modules/booksAtLs.js'; // eslint-disable-line no-unused-vars
 // Books removal funtion from ui & localstorage
-import removeBook from "./modules/removeBook.js";
+import removeBook from './modules/removeBook.js';
 // One page navigation
-import onePageNavigation from "./modules/onePageNavigation.js";
+import onePageNavigation from './modules/onePageNavigation.js';
 // //Get Date & time
-import getDate from "./modules/getDate.js";
+import getDate from './modules/getDate.js';
 // Invoking books representaion method
-document.addEventListener("DOMContentLoaded", BooksInDom.displayBooks);
+document.addEventListener('DOMContentLoaded', BooksInDom.displayBooks);
 
 // Add books in the Ui
 addBooksToUi();
